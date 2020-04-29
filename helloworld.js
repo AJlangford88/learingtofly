23a553a5e5e39c5393afeaf3b298aa6001aabab7
AJlangford88 <57740853+AJlangford88@users.noreply.github.com>
2020-04-28 22:05:19 -0700

    coding lesson 1
    timothy is great

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+console.log('hello world');
+
+var pen15 = "8==D";
+console.log(pen15);
+console.log(pen15.length);
+var myage= 5000
+console.log(myage)
+myage.toString()
+var dogisbarking = true;
+console.log(dogisbarking)
+var dogisnotbarking= false;
+console.log(dogisnotbarking);
